976ee618c76361c4e30d732ece17302fff244692
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-01-19 23:12:52 +0200

    добавляет решение задачи №3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const fornRef = document.querySelector('.form');
+const delayRef = document.querySelector('[name="delay"]');
+const stepRef = document.querySelector('[name="step"]');
+const amountRef = document.querySelector('[name="amount"]');
+
+fornRef.addEventListener('submit', runCreatePromises)
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position,delay});
+      }
+    }, delay);
+  });
+}
+
+function runCreatePromises(event) {
+  event.preventDefault();
+
+  let delay = +(delayRef.value);
+  const amount = +(amountRef.value);
+  const step = +(stepRef.value);
+  
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    delay += step;
   }
 }
